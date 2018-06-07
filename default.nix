2303ba924ca51346aaf92fcf2aca55a62d964198
John Boehr <john@johnboehr.is>
2018-06-07 11:26:54 -0700

    Working on getting carnix working

--- default.nix
+++ default.nix
@@ -1,12 +1,21 @@
 let
   pkgs = import <nixpkgs> {};
-  cargo-vendor = (pkgs.callPackage (import ./cargo-vendor-deps.nix) {}).cargo_vendor {};
-  buildRustPackage = pkgs.callPackage (import <nixpkgs/build-support/rust>) {
-    #inherit cargo-vendor;
+  rustChannel = pkgs.rustChannelOf { date = "2018-05-15"; channel = "nightly"; };
+  rust = rustChannel.rust;
+  cargo = rustChannel.cargo;
+  buildRustCrate = pkgs.callPackage (import ./build-rust-crate.nix) {
+    rustc = rust;
   };
-in {
-  imageflow = pkgs.callPackage ./derivation.nix {
-    inherit buildRustPackage;
+  buildRustCrateHelpers = pkgs.callPackage ./build-rust-crate-helpers.nix { };
+in (pkgs.callPackage ./Cargo.nix {
+  inherit buildRustCrate buildRustCrateHelpers;
+}).imageflow_server."0.1.0".override {
+  crateOverrides = pkgs.defaultCrateOverrides // {
+    libpng-sys = attrs: {
+      buildInputs = [ pkgs.zlib ];
+    };
+    imageflow_c_components = attrs: {
+      buildInputs = [ pkgs.zlib ];
+    };
   };
-  inherit cargo-vendor;
 }
